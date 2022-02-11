7b4d7372f5bede76b5dc25ccdb6b3ef96043f21b
Fabian Lee <flee999@hotmail.com>
2022-02-11 15:45:28 -0500

    ssh config writing ok

--- gcloud/show-ssh.sh
+++ gcloud/show-ssh.sh
@@ -19,8 +19,8 @@ gcloud config set project $project_id
 pub1=$(gcloud compute instances describe vm-pub-10-0-90-0 --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone=$region-b)
 pub2=$(gcloud compute instances describe vm-pub-10-0-91-0 --format='get(networkInterfaces[0].accessConfigs[0].natIP)' --zone=$region-b)
 
-#priv1=$(gcloud compute instances describe vm-prv-10-0-100-0 --format='get(networkInterfaces.networkIP)' --zone=$region-b)
-#priv2=$(gcloud compute instances describe vm-prv-10-0-101-0 --format='get(networkInterfaces.networkIP)' --zone=$region-b)
+priv1=$(gcloud compute instances describe vm-prv-10-0-100-0 --format='get(networkInterfaces.networkIP)' --zone=$region-b)
+priv2=$(gcloud compute instances describe vm-prv-10-0-101-0 --format='get(networkInterfaces.networkIP)' --zone=$region-b)
 
 
 ssh_key="$(cd ..;pwd)/gcp-ssh"
@@ -29,10 +29,10 @@ ssh_key="$(cd ..;pwd)/gcp-ssh"
 touch ~/.ssh/config
 for bastion in $pub1 $pub2; do
   if grep -qe "Host $bastion" ~/.ssh/config; then
-    echo "bastion $bastion already in ssh config"
+    echo "bastion $bastion already in ~/.ssh/config"
   else
 
-    echo "need to add bastion $bastion to ssh config"
+    echo "need to add bastion $bastion to ~/.ssh/config"
     cat << EOF >> ~/.ssh/config
 Host $bastion
   ForwardAgent Yes
@@ -41,10 +41,10 @@ EOF
 
   fi
 done
-exit 0
 
 
 cat <<EOL
+
 ==============================
 ssh into public vm-pub-10-0-90-0
   ssh ubuntu@${pub1} -i $ssh_key
@@ -58,14 +58,6 @@ ssh into private vm-prv-10-0-100-0
 ssh into private vm-prv-10-0-101-0
   ssh -J ubuntu@${pub2} ubuntu@$priv2 -i $ssh_key
 ==============================
-
-# Add to ~/.ssh/config
-Host $pub1
-  ForwardAgent Yes
-  IdentityFile $ssh_key
-Host $pub2
-  ForwardAgent Yes
-  IdentityFile $ssh_key
 EOL
 
 
