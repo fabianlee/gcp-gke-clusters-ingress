fee46a898f8ce6358badd55aa4d65766d5b92eec
Fabian Lee <flee999@hotmail.com>
2022-02-14 09:09:41 -0500

    test curl script to test ingress

---
+++ roles/testapp/templates/test-curl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+caStr="--cacert /tmp/myCA.{{cluster_name}}.local.crt"
+
+echo "========== PUBLIC ===================="
+domain=my-primary.{{cluster_name}}.local
+resolveStr="--resolve $domain:443:{{subnet_prefix}}.198"
+set -x
+curl -v $caStr $resolveStr https://$domain/myhello/
+set +x
+
+echo ""
+echo "========== INTERNAL ===================="
+domain=my-secondary.{{cluster_name}}.local
+resolveStr="--resolve $domain:443:{{subnet_prefix}}.199"
+set -x
+curl -v $caStr $resolveStr https://$domain/myint/
+set +x
+
+
