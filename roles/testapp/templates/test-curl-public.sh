dba3ced32fd40c476265adad7a599977e0ebef8d
Fabian Lee <flee999@hotmail.com>
2022-02-14 22:15:43 -0500

    test for public IP ingress

---
+++ roles/testapp/templates/test-curl-public.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+caStr="--cacert /tmp/myCA.{{cluster_name}}.local.crt"
+
+publicIP=$(kubectl get ingress -n asm-gateways my-ingress -o=jsonpath="{.status.loadBalancer.ingress[0].ip}")
+[ -n "$publicIP" ] || { echo "ERROR could not find public IP for my-ingress"; exit 3; }
+
+echo "========== PUBLIC ===================="
+domain=my-primary.{{cluster_name}}.local
+resolveStr="--resolve $domain:443:$publicIP"
+set -x
+curl $caStr $resolveStr https://$domain/myhello/
+set +x
+
+
