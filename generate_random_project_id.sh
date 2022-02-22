ce5e422dc16fc8834dd7b977914917ee6ff5bdb0
Fabian Lee <flee999@hotmail.com>
2022-02-22 15:22:32 -0500

    change project unique id for terraform vars also

--- generate_random_project_id.sh
+++ generate_random_project_id.sh
@@ -5,6 +5,9 @@ RAN=$((1+ RANDOM%100000))
 echo "going to use unique GCP project id: $RAN"
 sed -i "s/project_id=.*/project_id=my-gkeproj1-$RAN/" global.properties
 sed -i "s/project_id:.*/project_id=my-gkeproj1-$RAN/" group_vars/all
+sed -i "s/project = .*/project = \"my-gkeproj1-$RAN\"/" tf/envs/all.tfvars
 
+echo "======== RESULTS ============"
 grep ^project_id global.properties
 grep ^project_id group_vars/all
+grep ^project tf/envs/all.tfvars
