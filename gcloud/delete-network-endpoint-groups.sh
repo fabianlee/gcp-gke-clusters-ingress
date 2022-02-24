14dceb66c06370531d8110c9bb14559b39da0435
Fabian Lee <flee999@hotmail.com>
2022-02-24 07:30:58 -0500

    NEG are zonal, so removed region logic. and fixed tail so that loop deletes all

--- gcloud/delete-network-endpoint-groups.sh
+++ gcloud/delete-network-endpoint-groups.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #
-# Deletes network endpoint groups orphaned by k8s clusters
+# Deletes zonal network endpoint groups orphaned by k8s clusters
 #
 
 project_id="$1"
 network_name="$2"
 region="$3"
-
 if [[ -z "$project_id" || -z "$network_name" || -z "$region" ]]; then
   echo "Usage: projectId networkName region"
   exit 3
@@ -17,19 +16,12 @@ fi
 # even standard gke clusters leaves one
 gcloud compute network-endpoint-groups list --project=$project_id
 
-# --regions does not mean those in its zones will be returned
-# and --regions does not list zone specific NEG
-# so instead we just show all in project
-for line in $(gcloud compute network-endpoint-groups list --project $project_id --format="csv(name,region,zone)" | tail -n1); do
+# show all in project
+for line in $(gcloud compute network-endpoint-groups list --project $project_id --format="csv(name,zone)" | tail -n+2); do
   neg_name=$(echo $line | cut -d, -f1)
-  region_full=$(echo $line | cut -d, -f2)
-  zone_full=$(echo $line | cut -d, -f3)
+  zone_full=$(echo $line | cut -d, -f2)
   set -x
-  if [ -z "$region_full" ]; then
-    gcloud compute network-endpoint-groups delete $neg_name --project=$project_id --zone=$zone_full --quiet
-  else
-    gcloud compute network-endpoint-groups delete $neg_name --project=$project_id --region=$region_full --quiet
-  fi
+  gcloud compute network-endpoint-groups delete $neg_name --project=$project_id --zone=$zone_full --quiet
   set +x
 done
 
